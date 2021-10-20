24ada3e7798fcea84b5d87ba250b422b63436d38
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-10-20 13:24:53 -0500

    Added folder variable.

--- gitlab_ci_tools_2/b64_util.py
+++ gitlab_ci_tools_2/b64_util.py
@@ -1,5 +1,6 @@
 import argparse
 import base64
+import os
 import pathlib
 from datetime import datetime
 
@@ -15,9 +16,9 @@ def decode(encrypted_text, encoding):
     return clear_content
 
 
-def write_files(encrypted_text, clear_content, environment, action, encoding):
-    enc_filename = write_clipboard(environment, action, 'enc', encrypted_text)
-    clear_filename = write_clipboard(environment, action, 'dec', clear_content.decode(encoding))
+def write_files(encrypted_text, clear_content, environment, action, encoding, target_folder):
+    enc_filename = write_clipboard(environment, action, 'enc', encrypted_text, target_folder)
+    clear_filename = write_clipboard(environment, action, 'dec', clear_content.decode(encoding), target_folder)
     return enc_filename, clear_filename
 
 
@@ -27,7 +28,7 @@ def encode_decode(args):
         clipboard_content = pyperclip.paste()
         clear_content = decode(clipboard_content, encoding)
         enc_filename, clear_filename = write_files(clipboard_content, clear_content, args.environment,
-                                                   args.action, encoding)
+                                                   args.action, encoding, args.folder)
         print(f'Encrypted file: {enc_filename}')
         print(f'Decrypted file: {clear_filename}')
     else:
@@ -35,14 +36,14 @@ def encode_decode(args):
         encrypted_content = base64.b64encode(clipboard_content.encode(encoding)).decode(encoding)
         print(encrypted_content)
         pyperclip.copy(encrypted_content)
-        enc_filename = write_clipboard(args.environment, args.action, 'enc', encrypted_content)
+        enc_filename = write_clipboard(args.environment, args.action, 'enc', encrypted_content, args.folder)
         print(f'Encrypted file: {enc_filename}')
 
 
-def write_clipboard(environment, source, source_type, content):
-    target_path = pathlib.Path(__file__).parent
+def write_clipboard(environment, source, source_type, content, target_path):
+    # target_path = pathlib.Path(__file__).parent
     times_stamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-    clip_file = target_path / f'{times_stamp}-{source}-{environment}-{source_type}.txt'
+    clip_file = os.path.join(target_path, f'{times_stamp}-{source}-{environment}-{source_type}.txt')
     with open(clip_file, 'w') as c_file:
         c_file.write(content)
     return clip_file
