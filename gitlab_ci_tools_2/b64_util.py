04c797b468a106d1f546c9a6a04ed6aea59342d9
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-10-19 22:09:10 -0500

    Environment configuration.

---
+++ gitlab_ci_tools_2/b64_util.py
@@ -0,0 +1,55 @@
+import argparse
+import base64
+import pathlib
+from datetime import datetime
+
+import pyperclip
+
+
+def encode(clear_text):
+    pass
+
+
+def decode(encrypted_text):
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action', choices=['encrypt', 'decrypt'])
+    parser.add_argument('environment', choices=['staging', 'prod'])
+    args = parser.parse_args()
+    encoding = 'utf-8'
+    if args.action == 'decrypt':
+        clipboard_content = pyperclip.paste()
+        enc_filename = write_clipboard(args.environment, args.action, 'enc', clipboard_content)
+        print(f'Encrypted file: {enc_filename}')
+        clear_content = base64.b64decode(clipboard_content.encode(encoding))
+        clear_filename = write_clipboard(args.environment, args.action, 'dec', clear_content.decode(encoding))
+        print(f'Decrypted file: {clear_filename}')
+    else:
+        clipboard_content = pyperclip.paste()
+        encrypted_content = base64.b64encode(clipboard_content.encode(encoding)).decode(encoding)
+        print(encrypted_content)
+        pyperclip.copy(encrypted_content)
+        enc_filename = write_clipboard(args.environment, args.action, 'enc', encrypted_content)
+        print(f'Encrypted file: {enc_filename}')
+
+
+
+def write_clipboard(environment, source, source_type, content):
+    target_path = pathlib.Path(__file__).parent
+    times_stamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    clip_file = target_path / f'{times_stamp}-{source}-{environment}-{source_type}.txt'
+    with open(clip_file, 'w') as c_file:
+        c_file.write(content)
+    return clip_file
+
+
+if __name__ == '__main__':
+    """
+    Copy encrypted text form CI
+    run:
+        python output/b64_util.py decrypt staging
+    """
+    main()
